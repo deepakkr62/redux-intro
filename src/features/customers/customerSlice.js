b11a780d8953d57a577a6ec3f52ab0010f5daab5
deepakkr62 <deepakkr11113@gmail.com>
2026-09-18 17:25:20 +0530

    professional redux file structure : State slice

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,38 @@
+const intialStateCustomer = {
+  fullName: "",
+  nationalId: "",
+  createdAt: "",
+};
+
+export default function reducerCustomer(state = intialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalId,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateCustomerName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalId) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalId, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateCustomerName(fullName) {
+  return {
+    type: "customer/updateCustomerName",
+    payload: fullName,
+  };
+}
