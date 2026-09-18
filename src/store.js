b9eaf8a3a8234fd270dc031e37cf3bd7482100ff
deepakkr62 <deepakkr11113@gmail.com>
2026-09-18 15:42:34 +0530

    working with action creators

--- src/store.js
+++ src/store.js
@@ -40,17 +40,60 @@ function reducer(state = intialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "buy a car" },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function Deposit(amount) {
+  return {
+    type: "account/deposit",
+    payload: amount,
+  };
+}
+
+function Withdraw(amount) {
+  return {
+    type: "account/withdraw",
+    payload: amount,
+  };
+}
+
+function RequestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function PayLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+store.dispatch(Deposit(500));
 console.log(store.getState());
 
-store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch(Withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "buy a car" },
-});
+store.dispatch(RequestLoan(1000, "buy a car"));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(PayLoan());
 console.log(store.getState());
+
+//in old base they use const ACCOOUNT_DEPOSITE=accoutn/deposite and replace the
+// string with the constant in the reducer and action creator. This is a good
+// practice to avoid typos and make it easier to manage action types.
